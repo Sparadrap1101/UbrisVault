4d52d4a1985dfab885e15d099102aefc51508718
sparadrap <alexis111@orange.fr>
2022-08-18 23:37:29 +0200

    Add unit test for depositFunds() and withdrawFunds() with ERC20 mock

--- test/unit/UbrisVault.test.js
+++ test/unit/UbrisVault.test.js
@@ -1,49 +1,101 @@
 const { assert, expect } = require("chai");
-const { network, getNamedAccounts, deployments, ethers } = require("hardhat");
+const { ethers } = require("hardhat");
+const Erc20Token = require("/home/hhk/Desktop/UbrisVault/UbrisVault/artifacts/contracts/mocks/Erc20Token.sol/Erc20Token.json");
+const { BigNumber } = require("ethers");
 
 describe("UbrisVault Unit Tests", function () {
-  let vault, deployer, owner, addr1, addr2;
+  let vault, owner, addr1, addr2, token, ownerBalance, amount;
 
   beforeEach(async function () {
     [owner, addr1, addr2] = await ethers.getSigners();
     const Vault = await ethers.getContractFactory("UbrisVault");
     vault = await Vault.deploy();
+    await vault.deployed();
+
+    const Token = await ethers.getContractFactory("Erc20Token");
+    token = await Token.deploy("LINKCHAIN token", "LINK", BigNumber.from(100_000).mul((1e18).toString()));
+    await token.deployed();
+
+    await token.connect(owner).approve(vault.address, BigNumber.from(100_000).mul((1e18).toString()));
+    await token.connect(addr1).approve(vault.address, BigNumber.from(100_000).mul((1e18).toString()));
+    await token.connect(addr2).approve(vault.address, BigNumber.from(100_000).mul((1e18).toString()));
+
+    ownerBalance = (await token.balanceOf(owner.address)).toString();
+    amount = BigNumber.from(100).mul((1e18).toString()).toString();
   });
 
-  describe("depositFunds", function () {
-    it("Reverts when you enter address(0)", async function () {
+  describe("depositFunds() :", function () {
+    it("Should reverts when you enter address(0)", async function () {
       await expect(vault.depositFunds("0x0000000000000000000000000000000000000000", 1)).to.be.revertedWith(
         "This token doesn't exist."
       );
     });
 
-    /*
-    it("Reverts when you don't pay enough", async function () {
-      await expect(vault.depositFunds()).to.be.revertedWith("Lottery__NotEnoughETHEntered");
+    it("Should transfer funds to the contract", async function () {
+      assert.equal((await token.balanceOf(vault.address)).toString(), 0);
+      await vault.depositFunds(token.address, amount);
+      assert.equal((await token.balanceOf(vault.address)).toString(), amount);
     });
 
-    it("Records players when they enter", async function () {
-      await vault.enterLottery({ value: lotteryEntranceFee });
-      const playerFromContract = await lottery.getPlayer(0);
-      assert.equal(playerFromContract, deployer);
+    it("Should increments funds counter of the user in the protocol", async function () {
+      assert.equal((await vault.getUserBalance(owner.address, token.address)).toString(), 0);
+      await vault.depositFunds(token.address, amount);
+      assert.equal((await vault.getUserBalance(owner.address, token.address)).toString(), amount);
     });
 
-    it("Emits event on enter", async function () {
-      await expect(vault.enterLottery({ value: lotteryEntranceFee })).to.emit(lottery, "LotteryEnter");
+    it("Should emits an event", async function () {
+      await expect(vault.depositFunds(token.address, amount))
+        .to.emit(vault, "UserEnterProtocol")
+        .withArgs(owner.address, token.address, amount);
     });
 
-    it("Doesn't allow entrance when lottery is 'Calculating'", async function () {
-      // The only way to get lotteryState in `Calculating` mode is to call `performUpkeep` function.
-      await lottery.enterLottery({ value: lotteryEntranceFee });
-      // We increase time of our local blockchain in order to be able to call `performUpkeep` without
-      // waiting for our interval time. Then we mine 1 block, check HardHat docs for more.
-      await network.provider.send("evm_increaseTime", [interval.toNumber() + 1]);
-      await network.provider.send("evm_mine", []);
-      // We pretend to be a Chainlink Keeper to activate `performUpkeep` function and change lotteryState.
-      await lottery.performUpkeep([]);
-      // Now lotteryState has changed to `Calculating` so we can perform our test.
-      await expect(lottery.enterLottery({ value: lotteryEntranceFee })).to.be.revertedWith("Lottery__NotOpen");
+    // await network.provider.send("evm_increaseTime", [interval.toNumber() + 1]);
+    // await network.provider.send("evm_mine", []);
+  });
+
+  describe("withdrawFunds() :", function () {
+    it("Should reverts when you enter address(0)", async function () {
+      await expect(vault.withdrawFunds("0x0000000000000000000000000000000000000000", 1)).to.be.revertedWith(
+        "This token doesn't exist."
+      );
     });
-    */
+
+    it("Should reverts if user haven't enough funds on the protocol", async function () {
+      await expect(vault.withdrawFunds(token.address, amount)).to.be.revertedWith(
+        "You can't withdraw more than your wallet funds."
+      );
+    });
+
+    // Add "it should reverts if not enough funds on the protocol" later.
+
+    it("Should transfer funds back to the user", async function () {
+      await vault.depositFunds(token.address, amount);
+      assert.equal((await token.balanceOf(owner.address)).toString(), ownerBalance - amount);
+
+      await vault.withdrawFunds(token.address, amount);
+      assert.equal((await token.balanceOf(owner.address)).toString(), ownerBalance);
+    });
+
+    it("Should not transfer funds of another user", async function () {
+      await vault.depositFunds(token.address, amount);
+      await expect(vault.connect(addr1).withdrawFunds(token.address, amount)).to.be.revertedWith(
+        "You can't withdraw more than your wallet funds."
+      );
+    });
+
+    it("Should decrements funds counter of the user in the protocol", async function () {
+      await vault.depositFunds(token.address, amount);
+      assert.equal((await vault.getUserBalance(owner.address, token.address)).toString(), amount);
+
+      await vault.withdrawFunds(token.address, amount);
+      assert.equal((await vault.getUserBalance(owner.address, token.address)).toString(), 0);
+    });
+
+    it("Should emits an event", async function () {
+      await vault.depositFunds(token.address, amount);
+      await expect(vault.withdrawFunds(token.address, amount))
+        .to.emit(vault, "UserExitProtocol")
+        .withArgs(owner.address, token.address, amount);
+    });
   });
 });
