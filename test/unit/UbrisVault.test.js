f961ba7ac64be329489d2c3dc208f13318a5c6bd
sparadrap <alexis111@orange.fr>
2022-08-19 02:09:04 +0200

    Deploy strategy & add test for addStrategy(), removeStrategy(), pauseStrategy() & resumeStrategy()

--- test/unit/UbrisVault.test.js
+++ test/unit/UbrisVault.test.js
@@ -4,7 +4,7 @@ const Erc20Token = require("/home/hhk/Desktop/UbrisVault/UbrisVault/artifacts/co
 const { BigNumber } = require("ethers");
 
 describe("UbrisVault Unit Tests", function () {
-  let vault, owner, addr1, addr2, token, ownerBalance, amount;
+  let vault, strategy, token, owner, addr1, addr2, ownerBalance, amount;
 
   beforeEach(async function () {
     [owner, addr1, addr2] = await ethers.getSigners();
@@ -15,6 +15,10 @@ describe("UbrisVault Unit Tests", function () {
     const Token = await ethers.getContractFactory("Erc20Token");
     token = await Token.deploy("LINKCHAIN token", "LINK", BigNumber.from(100_000).mul((1e18).toString()));
     await token.deployed();
+
+    const Strategy = await ethers.getContractFactory("StrategyTest");
+    strategy = await Strategy.deploy(token.address);
+    await strategy.deployed();
 
     await token.connect(owner).approve(vault.address, BigNumber.from(100_000).mul((1e18).toString()));
     await token.connect(addr1).approve(vault.address, BigNumber.from(100_000).mul((1e18).toString()));
@@ -96,6 +100,181 @@ describe("UbrisVault Unit Tests", function () {
       await expect(vault.withdrawFunds(token.address, amount))
         .to.emit(vault, "UserExitProtocol")
         .withArgs(owner.address, token.address, amount);
+    });
+  });
+
+  describe("addStrategy() :", function () {
+    it("Should reverts if the user is not the owner of the protocol", async function () {
+      await expect(vault.connect(owner).addStrategy(strategy.address, "Hey")).to.not.be.reverted;
+      await expect(vault.connect(addr1).addStrategy(strategy.address, "Hey")).to.be.reverted;
+    });
+
+    it("Should reverts when you enter address(0)", async function () {
+      await expect(vault.addStrategy("0x0000000000000000000000000000000000000000", "Hey")).to.be.revertedWith(
+        "This strategy doesn't exist."
+      );
+    });
+
+    it("Should reverts if the strategy address is already whitelist", async function () {
+      await vault.addStrategy(strategy.address, "Best Strategy");
+      await expect(vault.addStrategy(strategy.address, "Best Strategy2")).to.be.revertedWith(
+        "This strategy is already whitelist."
+      );
+    });
+
+    it("Should add the good name to the strategy arguments", async function () {
+      const name = "Best Strategy";
+      await vault.addStrategy(strategy.address, name);
+      assert.equal(await vault.getStrategyName(strategy.address), name);
+    });
+
+    it("Should update the good state to the strategy arguments", async function () {
+      assert.equal(await vault.getStrategyState(strategy.address), 0);
+      const name = "Best Strategy";
+      await vault.addStrategy(strategy.address, name);
+      assert.equal(await vault.getStrategyState(strategy.address), 1);
+    });
+
+    it("Should add the strategy on the whitelist in its arguments", async function () {
+      assert.equal(await vault.isStrategyWhitelist(strategy.address), false);
+      const name = "Best Strategy";
+      await vault.addStrategy(strategy.address, name);
+      assert.equal(await vault.isStrategyWhitelist(strategy.address), true);
+    });
+
+    it("Should emits an event", async function () {
+      const name = "Best Strategy";
+      await expect(vault.addStrategy(strategy.address, name)).to.emit(vault, "NewStrategy").withArgs(strategy.address, name);
+    });
+  });
+
+  describe("removeStrategy() :", function () {
+    it("Should reverts if the user is not the owner of the protocol", async function () {
+      const name = "Best Strategy";
+      await vault.addStrategy(strategy.address, name);
+
+      await expect(vault.connect(owner).removeStrategy(strategy.address)).to.not.be.reverted;
+      await expect(vault.connect(addr1).removeStrategy(strategy.address)).to.be.reverted;
+    });
+
+    it("Should reverts when you enter address(0)", async function () {
+      await expect(vault.removeStrategy("0x0000000000000000000000000000000000000000")).to.be.revertedWith(
+        "This strategy doesn't exist."
+      );
+    });
+
+    it("Should be in whitelist to be removed", async function () {
+      await expect(vault.removeStrategy(strategy.address)).to.be.revertedWith("This strategy has already been removed.");
+    });
+
+    it("Should update the good state to the strategy arguments", async function () {
+      const name = "Best Strategy";
+      await vault.addStrategy(strategy.address, name);
+      assert.equal(await vault.getStrategyState(strategy.address), 1);
+      await vault.removeStrategy(strategy.address);
+      assert.equal(await vault.getStrategyState(strategy.address), 0);
+    });
+
+    it("Should removed the strategy of the whitelist in its arguments", async function () {
+      const name = "Best Strategy";
+      await vault.addStrategy(strategy.address, name);
+      assert.equal(await vault.isStrategyWhitelist(strategy.address), true);
+      await vault.removeStrategy(strategy.address);
+      assert.equal(await vault.isStrategyWhitelist(strategy.address), false);
+    });
+
+    it("Should emits an event", async function () {
+      const name = "Best Strategy";
+      await vault.addStrategy(strategy.address, name);
+      await expect(vault.removeStrategy(strategy.address))
+        .to.emit(vault, "StrategyRemoved")
+        .withArgs(strategy.address, name);
+    });
+  });
+
+  describe("pauseStrategy() :", function () {
+    it("Should reverts if the user is not the owner of the protocol", async function () {
+      const name = "Best Strategy";
+      await vault.addStrategy(strategy.address, name);
+
+      await expect(vault.connect(owner).pauseStrategy(strategy.address)).to.not.be.reverted;
+      await expect(vault.connect(addr1).pauseStrategy(strategy.address)).to.be.reverted;
+    });
+
+    it("Should reverts when you enter address(0)", async function () {
+      await expect(vault.pauseStrategy("0x0000000000000000000000000000000000000000")).to.be.revertedWith(
+        "This strategy doesn't exist."
+      );
+    });
+
+    it("Should reverts if the strategy is not whitelist", async function () {
+      await expect(vault.pauseStrategy(strategy.address)).to.be.revertedWith("This strategy is not whitelist.");
+    });
+
+    it("Should reverts if the state of the strategy is already CLOSE", async function () {
+      const name = "Best Strategy";
+      await vault.addStrategy(strategy.address, name);
+      await vault.pauseStrategy(strategy.address);
+      await expect(vault.pauseStrategy(strategy.address)).to.be.revertedWith("This strategy is already in pause.");
+    });
+
+    it("Should update the state of the strategy to CLOSE", async function () {
+      const name = "Best Strategy";
+      await vault.addStrategy(strategy.address, name);
+      assert.equal(await vault.getStrategyState(strategy.address), 1);
+      await vault.pauseStrategy(strategy.address);
+      assert.equal(await vault.getStrategyState(strategy.address), 0);
+    });
+
+    it("Should emits an event", async function () {
+      const name = "Best Strategy";
+      await vault.addStrategy(strategy.address, name);
+      await expect(vault.pauseStrategy(strategy.address)).to.emit(vault, "StrategyPaused").withArgs(strategy.address, name);
+    });
+  });
+
+  describe("resumeStrategy() :", function () {
+    it("Should reverts if the user is not the owner of the protocol", async function () {
+      const name = "Best Strategy";
+      await vault.addStrategy(strategy.address, name);
+      await vault.pauseStrategy(strategy.address);
+
+      await expect(vault.connect(owner).resumeStrategy(strategy.address)).to.not.be.reverted;
+      await expect(vault.connect(addr1).resumeStrategy(strategy.address)).to.be.reverted;
+    });
+
+    it("Should reverts when you enter address(0)", async function () {
+      await expect(vault.resumeStrategy("0x0000000000000000000000000000000000000000")).to.be.revertedWith(
+        "This strategy doesn't exist."
+      );
+    });
+
+    it("Should reverts if the strategy is not whitelist", async function () {
+      await expect(vault.resumeStrategy(strategy.address)).to.be.revertedWith("This strategy is not whitelist.");
+    });
+
+    it("Should reverts if the state of the strategy is already OPEN", async function () {
+      const name = "Best Strategy";
+      await vault.addStrategy(strategy.address, name);
+      await expect(vault.resumeStrategy(strategy.address)).to.be.revertedWith("This strategy is already active.");
+    });
+
+    it("Should update the state of the strategy to OPEN", async function () {
+      const name = "Best Strategy";
+      await vault.addStrategy(strategy.address, name);
+      await vault.pauseStrategy(strategy.address);
+      assert.equal(await vault.getStrategyState(strategy.address), 0);
+      await vault.resumeStrategy(strategy.address);
+      assert.equal(await vault.getStrategyState(strategy.address), 1);
+    });
+
+    it("Should emits an event", async function () {
+      const name = "Best Strategy";
+      await vault.addStrategy(strategy.address, name);
+      await vault.pauseStrategy(strategy.address);
+      await expect(vault.resumeStrategy(strategy.address))
+        .to.emit(vault, "StrategyResumed")
+        .withArgs(strategy.address, name);
     });
   });
 });
