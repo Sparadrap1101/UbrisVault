32f6d04a7d671fe30d558d8e8d20756b1fe4fe76
sparadrap <alexis111@orange.fr>
2022-10-04 12:36:47 +0200

    Add Mumbai Testnet network

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,6 +7,7 @@ require("hardhat-contract-sizer");
 require("dotenv").config();
 
 const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL || "https://eth-rinkeby.alchemyapi";
+const MUMBAI_RPC_URL = process.env.MUMBAI_RPC_URL || "https://eth-mumbai.alchemyapi";
 const PRIVATE_KEY = process.env.PRIVATE_KEY || "0x";
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "Your etherscan API key";
 const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "key";
@@ -23,6 +24,12 @@ module.exports = {
       chainId: 4,
       blockConfirmations: 6,
       url: RINKEBY_RPC_URL,
+      accounts: [PRIVATE_KEY],
+    },
+    mumbai: {
+      chainId: 80001,
+      blockConfirmations: 6,
+      url: MUMBAI_RPC_URL,
       accounts: [PRIVATE_KEY],
     },
   },
