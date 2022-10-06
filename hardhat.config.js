cb440c642a6ce28f20dfc92305e6180193cead2e
sparadrap <alexis111@orange.fr>
2022-10-06 20:07:38 +0200

    Add Polygon Network

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,6 +8,7 @@ require("dotenv").config();
 
 const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL || "https://eth-rinkeby.alchemyapi";
 const MUMBAI_RPC_URL = process.env.MUMBAI_RPC_URL || "https://eth-mumbai.alchemyapi";
+const POLYGON_RPC_URL = process.env.POLYGON_RPC_URL || "https://eth-polygon.alchemyapi";
 const PRIVATE_KEY = process.env.PRIVATE_KEY || "0x";
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "Your etherscan API key";
 const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "key";
@@ -30,6 +31,12 @@ module.exports = {
       chainId: 80001,
       blockConfirmations: 6,
       url: MUMBAI_RPC_URL,
+      accounts: [PRIVATE_KEY],
+    },
+    polygon: {
+      chainId: 137,
+      blockConfirmations: 6,
+      url: POLYGON_RPC_URL,
       accounts: [PRIVATE_KEY],
     },
   },
