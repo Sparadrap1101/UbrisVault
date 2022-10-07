ff48ff310ec2a5e6f2812c955775ca33a5a053ec
sparadrap <alexis111@orange.fr>
2022-10-07 23:52:23 +0200

    Aave Strategy deploy

--- deploy/01-deploy-ubrisvault.js
+++ deploy/01-deploy-ubrisvault.js
@@ -4,9 +4,17 @@ const { verify } = require("../utils/verify.js");
 module.exports = async function ({ getNamedAccounts, deployments }) {
   const { deploy, log } = deployments;
   const { deployer } = await getNamedAccounts();
-  const args = [];
+  /*const args = [];
 
   const ubrisvault = await deploy("UbrisVault", {
+    from: deployer,
+    args: args,
+    log: true,
+    waitConfirmations: 6,
+  });*/
+  const args = ["0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174", "0x794a61358d6845594f94dc1db02a252b5b4814ad"];
+
+  const aaveBasicStrategy = await deploy("AaveBasicStrategy", {
     from: deployer,
     args: args,
     log: true,
