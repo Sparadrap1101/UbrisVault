3067e3a0b9c4efc31f32ea2856dadb92c9552337
Sparadrap1101 <alexis111@orange.fr>
2022-10-11 20:40:47 +0200

    Add Uniswap script for testing interaction

--- scripts/interactAave.js
+++ scripts/interactAave.js
@@ -3,11 +3,13 @@ const { ethers, run, network } = require("hardhat");
 async function main() {
   console.log("Starting...");
   const accounts = await ethers.getSigners();
-  const contractAddress = "0xA9BDf7764DeeED5F75dCe4F986F9349Eb20C6469";
-  const assetAddress = "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174";
+  const contractAddress = "0x2c40566a76F8261f67452bCd3F15e832E23eB318";
+  const assetAddress = "0xA6FA4fB5f76172d178d61B04b0ecd319C5d1C0aa";
+  const assetAddress2 = "0x9c3C9283D3e44854697Cd22D3Faa240Cfb032889";
   const myContract = await hre.ethers.getContractAt("AaveBasicStrategy", contractAddress);
   const myToken = await hre.ethers.getContractAt("ERC20", assetAddress);
-  const amount = 100;
+  const myToken2 = await hre.ethers.getContractAt("ERC20", assetAddress2);
+  const amount = 1000000000000000;
 
   /*
   const hoho = await myToken.approve(contractAddress, amount);
@@ -46,8 +48,21 @@ async function main() {
   // const hihi = await myToken.transfer(contractAddress, amount);
   // await hihi.wait(1);
 
+  const hoho = await myToken.approve(contractAddress, amount);
+  await hoho.wait(1);
+
   let userBalance = await myToken.balanceOf(contractAddress);
-  console.log(`User balance: ${userBalance}`);
+  console.log(`WETH balance: ${userBalance}`);
+  let userBalance2 = await myToken2.balanceOf(contractAddress);
+  console.log(`WMATIC balance: ${userBalance2}`);
+
+  const hihi = await myContract.enterStrategy(assetAddress, accounts[0].address, amount);
+  await hihi.wait(1);
+
+  userBalance = await myToken.balanceOf(contractAddress);
+  console.log(`WETH balance: ${userBalance}`);
+  userBalance2 = await myToken2.balanceOf(contractAddress);
+  console.log(`WMATIC balance: ${userBalance2}`);
 
   console.log("a");
   const humhum = await myContract.test(amount);
@@ -56,7 +71,9 @@ async function main() {
   console.log("c");
 
   userBalance = await myToken.balanceOf(contractAddress);
-  console.log(`User balance: ${userBalance}`);
+  console.log(`WETH balance: ${userBalance}`);
+  userBalance2 = await myToken2.balanceOf(contractAddress);
+  console.log(`WMATIC balance: ${userBalance2}`);
 
   const test = await myContract.strategyTest();
   console.log(test);
