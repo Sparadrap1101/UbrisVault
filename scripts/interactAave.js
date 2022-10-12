366d4b4f50a27667be1c49f75b81a0f48a093a22
Sparadrap1101 <alexis111@orange.fr>
2022-10-12 20:21:56 +0200

    Interactions Uniswap

--- scripts/interactAave.js
+++ scripts/interactAave.js
@@ -3,13 +3,13 @@ const { ethers, run, network } = require("hardhat");
 async function main() {
   console.log("Starting...");
   const accounts = await ethers.getSigners();
-  const contractAddress = "0x2c40566a76F8261f67452bCd3F15e832E23eB318";
-  const assetAddress = "0xA6FA4fB5f76172d178d61B04b0ecd319C5d1C0aa";
-  const assetAddress2 = "0x9c3C9283D3e44854697Cd22D3Faa240Cfb032889";
+  const contractAddress = "0x58f7e38c245a82773F405871d26E1c08c961b43E";
+  const assetAddress = "0x9c3C9283D3e44854697Cd22D3Faa240Cfb032889";
+  const assetAddress2 = "0xA6FA4fB5f76172d178d61B04b0ecd319C5d1C0aa";
   const myContract = await hre.ethers.getContractAt("AaveBasicStrategy", contractAddress);
   const myToken = await hre.ethers.getContractAt("ERC20", assetAddress);
   const myToken2 = await hre.ethers.getContractAt("ERC20", assetAddress2);
-  const amount = 1000000000000000;
+  const amount = myToken.balanceOf(accounts[0].address);
 
   /*
   const hoho = await myToken.approve(contractAddress, amount);
@@ -56,7 +56,7 @@ async function main() {
   let userBalance2 = await myToken2.balanceOf(contractAddress);
   console.log(`WMATIC balance: ${userBalance2}`);
 
-  const hihi = await myContract.enterStrategy(assetAddress, accounts[0].address, amount);
+  const hihi = await myContract.enterStrategy(assetAddress, accounts[0].address, amount, false);
   await hihi.wait(1);
 
   userBalance = await myToken.balanceOf(contractAddress);
@@ -65,7 +65,7 @@ async function main() {
   console.log(`WMATIC balance: ${userBalance2}`);
 
   console.log("a");
-  const humhum = await myContract.test(amount);
+  const humhum = await myContract.swapOnUniswap(assetAddress, assetAddress2, amount, 3000);
   console.log("b");
   await humhum.wait(1);
   console.log("c");
