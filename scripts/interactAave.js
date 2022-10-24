928bd48dddbc75ba86a7931b7d69a1579272d84c
Sparadrap1101 <alexis111@orange.fr>
2022-10-24 14:26:07 +0200

    Interaction tests Goerli

--- scripts/interactAave.js
+++ scripts/interactAave.js
@@ -3,56 +3,56 @@ const { ethers, run, network } = require("hardhat");
 async function main() {
   console.log("Starting...");
   const accounts = await ethers.getSigners();
-  const contractAddress = "0x46494D43a18da104CA41c5816FD05CDeD04eAfB4";
+  const contractAddress = "0x6f3Fd5AA64A098c060AAA6b93cC97F2B2E56321d";
   const assetAddress = "0xA2025B15a1757311bfD68cb14eaeFCc237AF5b43";
   const assetAddress2 = "0xA2025B15a1757311bfD68cb14eaeFCc237AF5b43";
   const myContract = await hre.ethers.getContractAt("AaveBasicStrategy", contractAddress);
   const myToken = await hre.ethers.getContractAt("ERC20", assetAddress);
   const myToken2 = await hre.ethers.getContractAt("ERC20", assetAddress2);
   const amount = 1000;
-
+  /*
   let userBalance = await myToken.balanceOf(contractAddress);
   console.log(`WETH contract balance: ${userBalance}`);
-  userBalance = await myContract.getTotalStrategyBalance();
+  userBalance = await myContract.getTotalStrategyBalance().call();
   console.log(`WETH contract balance by contract function: ${userBalance}`);
   let userBalance2 = await myToken2.balanceOf(contractAddress);
   console.log(`\nWMATIC contract balance: ${userBalance2}`);
   userBalance = await myToken.balanceOf(accounts[0].address);
   console.log(`\nWETH user balance: ${userBalance}`);
-  userBalance2 = await myContract.getUserBalance(accounts[0].address);
+  userBalance2 = await myContract.getUserBalance(accounts[0].address).call();
   console.log(`WETH user balance by contract function: ${userBalance2}`);
   userBalance = await myContract.getStrategyATokenBalance();
   console.log(`\naToken contract balance by contract function: ${userBalance}`);
   userBalance = await myContract.getStrategyDebtTokenBalance();
   console.log(`debtToken contract balance by contract function: ${userBalance}`);
-  userBalance = await myContract.getPureStrategyBalanceOnAave();
+  userBalance = await myContract.getPureStrategyBalanceOnAave().call();
   console.log(`Pure contract balance on Aave by contract function: ${userBalance}`);
-  [userBalance, ,] = await myContract.getHealthFactor(0);
-  console.log(`\nHealthFactor contract by contract function: ${userBalance}`);
+  userBalance = await myContract.getHealthFactor(0);
+  console.log(`\nHealthFactor contract by contract function: ${userBalance[0]}`);
 
-  const hoho = await myToken.approve(contractAddress, amount);
+  const hoho = await myToken.approve(contractAddress, amount, { gasLimit: 1000000 });
   await hoho.wait(1);
   const hihi = await myContract.enterStrategy(assetAddress, accounts[0].address, amount);
-  await hihi.wait(1);
+  await hihi.wait(1);*/
 
   userBalance = await myToken.balanceOf(contractAddress);
   console.log(`\n\nWETH contract balance: ${userBalance}`);
   userBalance = await myContract.getTotalStrategyBalance();
-  console.log(`WETH contract balance by contract function: ${userBalance}`);
+  console.log(`WETH contract balance by contract function:`, parseInt(userBalance.data));
   userBalance2 = await myToken2.balanceOf(contractAddress);
   console.log(`\nWMATIC contract balance: ${userBalance2}`);
   userBalance = await myToken.balanceOf(accounts[0].address);
   console.log(`\nWETH user balance: ${userBalance}`);
   userBalance2 = await myContract.getUserBalance(accounts[0].address);
-  console.log(`WETH user balance by contract function: ${userBalance2}`);
+  console.log(`WETH user balance by contract function:`, parseInt(userBalance2.data));
   userBalance = await myContract.getStrategyATokenBalance();
   console.log(`\naToken contract balance by contract function: ${userBalance}`);
   userBalance = await myContract.getStrategyDebtTokenBalance();
   console.log(`debtToken contract balance by contract function: ${userBalance}`);
   userBalance = await myContract.getPureStrategyBalanceOnAave();
-  console.log(`Pure contract balance on Aave by contract function: ${userBalance}`);
-  [userBalance, ,] = await myContract.getHealthFactor(0);
-  console.log(`\nHealthFactor contract by contract function: ${userBalance}`);
+  console.log(`Pure contract balance on Aave by contract function:`, parseInt(userBalance.data));
+  //userBalance = await myContract.getHealthFactor(0);
+  //console.log(`\nHealthFactor contract by contract function:`, userBalance);
 
   /*
   let userBalance2 = await myToken2.balanceOf(contractAddress);
