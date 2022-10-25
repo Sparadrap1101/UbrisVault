494cd5c79b6246a62afd6b9c4ac52f23a0890d79
Sparadrap1101 <alexis111@orange.fr>
2022-10-25 20:51:22 +0200

    Add Aave Rewards functions in Aave Mock.

--- contracts/mocks/AaveMock.sol
+++ contracts/mocks/AaveMock.sol
@@ -159,4 +159,22 @@ contract AaveMock {
 
         return (totalCollateralBase, totalDebtBase, availableBorrowsBase, currentLiquidationThreshold, ltv, healthFactor);
     }
+
+    function claimAllRewardsToSelf(address[] calldata)
+        public
+        returns (address[] memory rewardsList, uint256[] memory claimedAmounts)
+    {
+        aToken.mint(msg.sender, 10);
+        supplyAmounts[msg.sender] += 10;
+
+        rewardsList = new address[](2);
+        rewardsList[0] = address(aToken);
+        rewardsList[1] = address(vToken);
+
+        claimedAmounts = new uint256[](2);
+        claimedAmounts[0] = 10;
+        claimedAmounts[1] = 0;
+
+        return (rewardsList, claimedAmounts);
+    }
 }
