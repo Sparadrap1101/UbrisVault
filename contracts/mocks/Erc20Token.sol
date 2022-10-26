9a242143d74ceb2de3a5ba71e6b745820cf69d59
Sparadrap1101 <alexis111@orange.fr>
2022-10-26 20:33:06 +0200

    Gas Optimisation: Modify import.

--- contracts/mocks/Erc20Token.sol
+++ contracts/mocks/Erc20Token.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.10;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Erc20Token is ERC20 {
     constructor(
