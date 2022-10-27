449bd53f03e5188b7bb7b62319f828e0f181368f
Sparadrap1101 <alexis111@orange.fr>
2022-10-27 18:07:34 +0200

    Add test for _strategy() function & internal mocks functions (Aave, Uniswap & Chainlink).

--- test/unit/AaveBasicStrategy.test.js
+++ test/unit/AaveBasicStrategy.test.js
@@ -6,30 +6,34 @@ const { BigNumber } = require("ethers");
 
 describe("\nAaveBasicStrategy Unit Tests\n", function () {
   let strategy,
-    token,
-    token2,
+    tokenA,
+    tokenB,
     aToken,
     vToken,
     owner,
     addr1,
     addr2,
-    ownerBalance,
     amount,
+    amountToBorrow,
     aave,
     uniswap,
     chainlinkA,
-    chainlinkB;
+    chainlinkB,
+    priceA,
+    priceB,
+    ratioSwap,
+    initialBalance;
 
   beforeEach(async function () {
     [owner, addr1, addr2] = await ethers.getSigners();
 
-    const Token = await ethers.getContractFactory("Erc20Token");
-    token = await Token.deploy("Ethereum token", "ETH", BigNumber.from(100_000).mul((1e18).toString()));
-    await token.deployed();
+    const TokenA = await ethers.getContractFactory("Erc20Token");
+    tokenA = await TokenA.deploy("Ethereum token", "ETH", BigNumber.from(100_000).mul((1e18).toString()));
+    await tokenA.deployed();
 
-    const Token2 = await ethers.getContractFactory("Erc20Token");
-    token2 = await Token2.deploy("USDC Token", "USDC", BigNumber.from(100_000).mul((1e18).toString()));
-    await token2.deployed();
+    const TokenB = await ethers.getContractFactory("Erc20Token");
+    tokenB = await TokenB.deploy("USDC Token", "USDC", BigNumber.from(100_000).mul((1e18).toString()));
+    await tokenB.deployed();
 
     const AToken = await ethers.getContractFactory("Erc20Token");
     aToken = await AToken.deploy("Aave Supply Token", "aToken", 0);
@@ -39,26 +43,30 @@ describe("\nAaveBasicStrategy Unit Tests\n", function () {
     vToken = await VToken.deploy("Aave Debt Token", "vToken", 0);
     await vToken.deployed();
 
-    const Aave = await ethers.getContractFactory("AaveMock");
-    aave = await Aave.deploy(token.address, token2.address, aToken.address, vToken.address);
-    await aave.deployed();
-
-    const Uniswap = await ethers.getContractFactory("UniswapMock");
-    uniswap = await Uniswap.deploy(token.address, token2.address, 2);
-    await uniswap.deployed();
-
     const ChainlinkA = await ethers.getContractFactory("ChainlinkMock");
-    chainlinkA = await ChainlinkA.deploy(token.address, 1500);
+    chainlinkA = await ChainlinkA.deploy(tokenA.address, 1500);
     await chainlinkA.deployed();
 
     const ChainlinkB = await ethers.getContractFactory("ChainlinkMock");
-    chainlinkB = await ChainlinkB.deploy(token.address, 1);
+    chainlinkB = await ChainlinkB.deploy(tokenB.address, 1);
     await chainlinkB.deployed();
+
+    [, priceA, , ,] = await chainlinkA.latestRoundData();
+    [, priceB, , ,] = await chainlinkB.latestRoundData();
+    ratioSwap = priceA / priceB;
+
+    const Aave = await ethers.getContractFactory("AaveMock");
+    aave = await Aave.deploy(tokenA.address, tokenB.address, aToken.address, vToken.address, ratioSwap);
+    await aave.deployed();
+
+    const Uniswap = await ethers.getContractFactory("UniswapMock");
+    uniswap = await Uniswap.deploy(tokenA.address, tokenB.address, ratioSwap);
+    await uniswap.deployed();
 
     const Strategy = await ethers.getContractFactory("InternalAaveBasicStrategy");
     strategy = await Strategy.deploy(
-      token.address,
-      token2.address,
+      tokenA.address,
+      tokenB.address,
       aave.address,
       aave.address,
       uniswap.address,
@@ -69,21 +77,209 @@ describe("\nAaveBasicStrategy Unit Tests\n", function () {
     );
     await strategy.deployed();
 
-    await token.connect(owner).approve(strategy.address, BigNumber.from(100_000).mul((1e18).toString()));
-    await token.connect(addr1).approve(strategy.address, BigNumber.from(100_000).mul((1e18).toString()));
-    await token.connect(addr2).approve(strategy.address, BigNumber.from(100_000).mul((1e18).toString()));
+    await tokenA.connect(owner).approve(strategy.address, BigNumber.from(100_000).mul((1e18).toString()));
+    await tokenA.connect(addr1).approve(strategy.address, BigNumber.from(100_000).mul((1e18).toString()));
+    await tokenA.connect(addr2).approve(strategy.address, BigNumber.from(100_000).mul((1e18).toString()));
 
-    await token2.connect(owner).approve(strategy.address, BigNumber.from(100_000).mul((1e18).toString()));
-    await token2.connect(addr1).approve(strategy.address, BigNumber.from(100_000).mul((1e18).toString()));
-    await token2.connect(addr2).approve(strategy.address, BigNumber.from(100_000).mul((1e18).toString()));
+    await tokenB.connect(owner).approve(strategy.address, BigNumber.from(100_000).mul((1e18).toString()));
+    await tokenB.connect(addr1).approve(strategy.address, BigNumber.from(100_000).mul((1e18).toString()));
+    await tokenB.connect(addr2).approve(strategy.address, BigNumber.from(100_000).mul((1e18).toString()));
 
-    ownerBalance = (await token.balanceOf(owner.address)).toString();
-    amount = BigNumber.from(100).mul((1e18).toString()).toString();
+    amount = 1000;
+    amountToBorrow = amount * ratioSwap;
   });
 
-  describe("\n-> enterStrategy() :", function () {
-    it("Should reverts when you enter address(0)", async function () {
-      expect((await strategy.chainlinkPriceFeed(true)).toString()).to.equal("1500");
+  describe("\n\nTest Mock functions :\n", function () {
+    beforeEach(async function () {
+      await tokenA.connect(owner).transfer(strategy.address, amount);
+    });
+
+    describe("\n-> _chainlinkPriceFeed() :", function () {
+      it("Should return price value.", async function () {
+        let [, priceA, , ,] = await chainlinkA.latestRoundData();
+        let [, priceB, , ,] = await chainlinkB.latestRoundData();
+        let result = priceA / priceB;
+
+        expect(await strategy.chainlinkPriceFeed(true)).to.equal(result);
+      });
+    });
+
+    describe("\n-> _supplyOnAavePool() :", function () {
+      it("Should decrease tokenA balance.", async function () {
+        initialBalance = await tokenA.balanceOf(strategy.address);
+        await strategy.supplyOnAavePool(tokenA.address, amount);
+
+        expect(await tokenA.balanceOf(strategy.address)).to.equal(initialBalance - amount);
+      });
+
+      it("Should increase aToken balance.", async function () {
+        initialBalance = await aToken.balanceOf(strategy.address);
+        await strategy.supplyOnAavePool(tokenA.address, amount);
+
+        expect(await aToken.balanceOf(strategy.address)).to.equal(initialBalance + amount);
+      });
+
+      it("Should increase supplyAmounts[] balance.", async function () {
+        initialBalance = await aave.supplyAmounts(strategy.address);
+        await strategy.supplyOnAavePool(tokenA.address, amount);
+
+        expect(await aave.supplyAmounts(strategy.address)).to.equal(parseInt(initialBalance) + parseInt(amount));
+      });
+    });
+
+    describe("\n-> _withdrawFromAavePool() :", function () {
+      it("Should decrease aToken balance.", async function () {
+        await strategy.supplyOnAavePool(tokenA.address, amount);
+        initialBalance = await aToken.balanceOf(strategy.address);
+        await strategy.withdrawFromAavePool(tokenA.address, amount);
+
+        expect(await aToken.balanceOf(strategy.address)).to.equal(initialBalance - amount);
+      });
+
+      it("Should increase tokenA balance.", async function () {
+        await strategy.supplyOnAavePool(tokenA.address, amount);
+        initialBalance = await tokenA.balanceOf(strategy.address);
+        await strategy.withdrawFromAavePool(tokenA.address, amount);
+
+        expect(await tokenA.balanceOf(strategy.address)).to.equal(initialBalance + amount);
+      });
+
+      it("Should decrease supplyAmounts[] balance.", async function () {
+        await strategy.supplyOnAavePool(tokenA.address, amount);
+        initialBalance = await aave.supplyAmounts(strategy.address);
+        await strategy.withdrawFromAavePool(tokenA.address, amount);
+
+        expect(await aave.supplyAmounts(strategy.address)).to.equal(initialBalance - amount);
+      });
+    });
+
+    describe("\n-> _borrowOnAave() :", function () {
+      it("Should increase vToken balance.", async function () {
+        await strategy.supplyOnAavePool(tokenA.address, amount);
+        initialBalance = await vToken.balanceOf(strategy.address);
+        await strategy.borrowOnAave(tokenB.address, amountToBorrow, 2);
+
+        expect(await vToken.balanceOf(strategy.address)).to.equal(initialBalance + amountToBorrow);
+      });
+
+      it("Should increase borrowAmounts[] balance.", async function () {
+        await strategy.supplyOnAavePool(tokenA.address, amount);
+        initialBalance = await aave.borrowAmounts(strategy.address);
+        await strategy.borrowOnAave(tokenB.address, amountToBorrow, 2);
+
+        expect(await aave.borrowAmounts(strategy.address)).to.equal(initialBalance + amountToBorrow);
+      });
+
+      it("Should increase tokenB balance.", async function () {
+        await strategy.supplyOnAavePool(tokenA.address, amount);
+        initialBalance = await tokenB.balanceOf(strategy.address);
+        await strategy.borrowOnAave(tokenB.address, amountToBorrow, 2);
+
+        expect(await tokenB.balanceOf(strategy.address)).to.equal(initialBalance + amountToBorrow);
+      });
+    });
+
+    describe("\n-> _repayOnAave() :", function () {
+      it("Should decrease vToken balance.", async function () {
+        await strategy.supplyOnAavePool(tokenA.address, amount);
+        await strategy.borrowOnAave(tokenB.address, amountToBorrow, 2);
+        initialBalance = await vToken.balanceOf(strategy.address);
+        await strategy.repayOnAave(tokenB.address, amountToBorrow, 2);
+
+        expect(await vToken.balanceOf(strategy.address)).to.equal(initialBalance - amountToBorrow);
+      });
+
+      it("Should decrease borrowAmounts[] balance.", async function () {
+        await strategy.supplyOnAavePool(tokenA.address, amount);
+        await strategy.borrowOnAave(tokenB.address, amountToBorrow, 2);
+        initialBalance = await aave.borrowAmounts(strategy.address);
+        await strategy.repayOnAave(tokenB.address, amountToBorrow, 2);
+
+        expect(await aave.borrowAmounts(strategy.address)).to.equal(initialBalance - amountToBorrow);
+      });
+
+      it("Should decrease tokenB balance.", async function () {
+        await strategy.supplyOnAavePool(tokenA.address, amount);
+        await strategy.borrowOnAave(tokenB.address, amountToBorrow, 2);
+        initialBalance = await tokenB.balanceOf(strategy.address);
+        await strategy.repayOnAave(tokenB.address, amountToBorrow, 2);
+
+        expect(await tokenB.balanceOf(strategy.address)).to.equal(initialBalance - amountToBorrow);
+      });
+    });
+
+    describe("\n-> _repayWithATokenOnAave() :", function () {
+      it("Should decrease vToken balance.", async function () {
+        await strategy.supplyOnAavePool(tokenA.address, amount);
+        await strategy.borrowOnAave(tokenB.address, amountToBorrow, 2);
+        initialBalance = await vToken.balanceOf(strategy.address);
+        await strategy.repayWithATokenOnAave(tokenB.address, amount, 2);
+
+        expect(await vToken.balanceOf(strategy.address)).to.equal(initialBalance - amountToBorrow);
+      });
+
+      it("Should decrease borrowAmounts[] balance.", async function () {
+        await strategy.supplyOnAavePool(tokenA.address, amount);
+        await strategy.borrowOnAave(tokenB.address, amountToBorrow, 2);
+        initialBalance = await aave.borrowAmounts(strategy.address);
+        await strategy.repayWithATokenOnAave(tokenB.address, amount, 2);
+
+        expect(await aave.borrowAmounts(strategy.address)).to.equal(initialBalance - amountToBorrow);
+      });
+
+      it("Should decrease aToken balance.", async function () {
+        await strategy.supplyOnAavePool(tokenA.address, amount);
+        await strategy.borrowOnAave(tokenB.address, amountToBorrow, 2);
+        initialBalance = await aToken.balanceOf(strategy.address);
+        await strategy.repayWithATokenOnAave(tokenB.address, amount, 2);
+
+        expect(await aToken.balanceOf(strategy.address)).to.equal(initialBalance - amount);
+      });
+
+      it("Should decrease supplyAmounts[] balance.", async function () {
+        await strategy.supplyOnAavePool(tokenA.address, amount);
+        await strategy.borrowOnAave(tokenB.address, amountToBorrow, 2);
+        initialBalance = await aave.supplyAmounts(strategy.address);
+        await strategy.repayWithATokenOnAave(tokenB.address, amount, 2);
+
+        expect(await aave.supplyAmounts(strategy.address)).to.equal(initialBalance - amount);
+      });
+    });
+
+    describe("\n-> _swapOnUniswap() :", function () {
+      it("Should decrease tokenA balance.", async function () {
+        initialBalance = await tokenA.balanceOf(strategy.address);
+        await strategy.swapOnUniswap(tokenA.address, tokenB.address, amount, true);
+
+        expect(await tokenA.balanceOf(strategy.address)).to.equal(initialBalance - amount);
+      });
+
+      it("Should increase tokenB balance.", async function () {
+        initialBalance = await tokenB.balanceOf(strategy.address);
+        await strategy.swapOnUniswap(tokenA.address, tokenB.address, amount, true);
+
+        expect(await tokenB.balanceOf(strategy.address)).to.equal(initialBalance + amountToBorrow);
+      });
+    });
+  });
+
+  describe("\n-> _strategy() :", function () {
+    beforeEach(async function () {
+      await tokenA.connect(owner).transfer(strategy.address, amount);
+      initialBalance = await tokenA.balanceOf(strategy.address);
+      await strategy.strategy(amount);
+    });
+
+    it("Should have increase aToken balance by : amount + (3/4 * amount), with leverage.", async function () {
+      expect(await aToken.balanceOf(strategy.address)).to.equal((amount * 7) / 4);
+    });
+
+    it("Should have increase vToken balance.", async function () {
+      expect(await vToken.balanceOf(strategy.address)).to.equal(((amount * 3) / 4) * ratioSwap);
+    });
+
+    it("Should have decrease tokenA balance.", async function () {
+      expect(await tokenA.balanceOf(strategy.address)).to.equal(initialBalance - amount);
     });
   });
 
